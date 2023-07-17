d41aa9d5bde3b6dbf1d4d9a073f2ae85fc0be517
vtrd2 <vh003827@cfjl.com.br>
2023-07-16 22:27:18 -0300

    pag 100

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo() {
+    return (
+        <a href="#">
+            <h1 className="logo">
+                <span>Editora MaujorTec</span>
+            </h1>
+        </a>
+    )
+}
+
+export default Logo
---
+++ src/components/Navegacao.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navegacao() {
+    return (
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Frontend</a></li>
+            <li><a href="#">Programacao</a></li>
+            <li><a href="#">Design</a></li>
+            <li><a href="#">Catálogo</a></li>
+        </ul>
+    )
+}
+
+export default Navegacao
